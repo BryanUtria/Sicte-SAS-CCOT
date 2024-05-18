d92e6a8435c3ad22d8db1ecac0fcf5068d59d84b
BryanUtria <brayan.kastel@gmail.com>
2024-05-18 08:07:09 -0500

    Modificaciones en navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css'
 import {FaBars, FaTimes, FaHome, FaFileInvoice, FaIndustry, FaChartLine, FaStar, FaTools, FaBuilding, FaChevronDown, FaChevronUp} from 'react-icons/fa';
 
 export const Navbar = () => {
-    const [showMobileMenu, setShowMobileMenu] = useState(true);
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
     const [showDropdownFacturacion, setShowDropdownFacturacion] = useState(false);
     const [showDropdownProduccion, setShowDropdownProduccion] = useState(false);
     const [showDropdownIndicadores, setShowDropdownIndicadores] = useState(false);
